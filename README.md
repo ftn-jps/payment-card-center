db14b38f599336752db97bc0d773754fd2b1a6a8
BojanStipic <bojanstipic@gmail.com>
2018-11-16 22:03:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Payment card center simulation server
+
+## Compiling from source
+
+```
+./mvnw package
+```
+
+## Usage
+
+```
+java -jar target/*.jar
+```
+
+Server listens on port 8082 by default. Use command line option --server.port= to change it.
---
+++ src/main/java/ftnjps/paymentcardcenter/PaymentCardCenterApplication.java
@@ -0,0 +1,12 @@
+package ftnjps.paymentcardcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCardCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentCardCenterApplication.class, args);
+	}
+}
